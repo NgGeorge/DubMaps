47bb222847951d7738ed0fb13e050e4d993b1079
George Ng <gngncp@uw.edu>
2016-10-15 21:06:21 -0700

    Map is properly working

--- js/map.js
+++ js/map.js
@@ -1,7 +1,6 @@
 var drawMap = function() {
 	L.mapbox.accessToken = 'pk.eyJ1IjoiZ25nY3AiLCJhIjoiY2lsNXd5b3ZrMDA0a3UybHoxY3h5NGN3eiJ9.OrXfMbZ123f3f1EfPRCHHA';
 	var map = L.mapbox.map('map', 'gngcp.p97o5d8j', {
-		maxBounds: bounds,
 	    	maxZoom: 20,
 	    	minZoom: 3
 	}).setView([40, -97], 5);
