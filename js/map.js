f9929620b68da449181d909fa218c67e7bc441a8
George Ng <gngncp@uw.edu>
2016-10-16 06:35:34 -0700

    Functioning Modals

--- js/map.js
+++ js/map.js
@@ -12,13 +12,11 @@ var drawMap = function() {
 	  maxZoom: 18,
 	  minZoom: 16,
 	}).setView([47.653800, -122.307851], 17);
-	var layer = L.mapbox.tileLayer('gngcp.p97o5d8j');
-	layer.on('ready', function(){
-	});
 
   for (i = 0; i < buildingLocations.length; i++) {
     var circle = L.circleMarker([buildingLocations[i].lat, buildingLocations[i].long]).addTo(map);
     circle.setRadius('20');
+    circle.on('click', function(){$('#myModal').modal()});
   }
 
   overlayTiles(map);
